98b996fcf5fa574eba9bfaa37bfcb61b071f6316
Himanshu Savargaonkar <savargaonkar123@gmail.com>
2022-01-14 12:52:35 +0530

    Version1.0

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Work_Tracker
 A program that keeps track of what you are doing.
+
+Track how much time you spend on doing what on your laptop!!!
+
+You get the activity log and a end of the log for doing further analysis on yourself ^_~
+
+We no longer need big cooperations, we can do data-mining on ourself now!!!
---
+++ bw_main.py
@@ -0,0 +1,268 @@
+import tkinter as tk
+from tkinter import * 
+import tkinter.font as tkFont
+import datetime
+
+#Tkinter Init
+root = tk.Tk()
+root.title("Work Monitor")
+#setting title
+root.title("Work Monitor")
+#setting window size
+width=370
+height=245
+screenwidth = root.winfo_screenwidth()
+screenheight = root.winfo_screenheight()
+alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+root.geometry(alignstr)
+root.config(bg="white")
+
+#Global Variables
+# topics = ["Capstone","DIP Research","Streben","IELTS","AEC Course","College Stuff","Projects"]
+topics = []
+topStatus = []
+topTime = []
+flag_screen2 = 0
+startTime = datetime.time()
+activeTopic = StringVar()
+pastTime = datetime.time()
+On = False
+
+
+
+def clearAll():
+    global flag_screen2
+    flag_screen2 = 0
+    for frame in root.winfo_children():
+        frame.destroy()
+
+
+
+def screen1():
+    clearAll()
+    
+    # topStatus = ["Not Started","Not Started","Not Started","Not Started","Not Started","Not Started","Not Started"]
+    # topTime = [datetime.time(),datetime.time(),datetime.time(),datetime.time(),datetime.time(),datetime.time(),datetime.time()]
+
+    
+    titleFrame = Frame(root)
+    titleFrame.pack()
+    titleFrame.config(bg="white")
+    welcomeLabel = Label(titleFrame, text="Good Morning", font =('arial', 20, 'bold'), bg="white", fg="black")
+    welcomeLabel.pack(padx=26, pady=30)
+
+    startButton = Button(titleFrame, text="Start DAY!!", command=startButton_command, 
+                            width = 10, height = 2,bg="white", fg="black", 
+                            font = ('arial', 10, 'bold'), activebackground="black", activeforeground="white")
+    startButton.pack(padx=26, pady=30), 
+
+
+def screen2():
+    global flag_screen2
+    clearAll()
+    flag_screen2 = 1
+    height = 200 + (len(topics) * 30)
+    geo = "600x" + str(height)
+    root.geometry(geo)
+    
+    #Frame to dislplay Date and Time
+    workFrame = Frame(root)
+    workFrame.pack()
+    workFrame.config(bg="white")
+    dateLabel = Label(workFrame, text="Date:", font =('arial', 14, 'bold'), bg="white", fg="black")
+    dateoutLabel = Label(workFrame, text="########", font =('arial', 14, 'bold'), bg="white", fg="black")
+    timeLabel = Label(workFrame, text="Time:", font =('arial', 14, 'bold'), bg="white", fg="black")
+    timeoutLabel = Label(workFrame, text="########", font =('arial', 14, 'bold'), bg="white", fg="black")
+    dateLabel.grid(row=0, column=0, padx=10, pady=10)
+    dateoutLabel.grid(row=0, column=1, padx=10, pady=10)
+    timeLabel.grid(row=0, column=2, padx=10, pady=10)
+    timeoutLabel.grid(row=0, column=3, padx=10, pady=10)
+
+    #Frame to set the current activity
+    clicked = StringVar()
+    activityFrame = Frame(root)
+    activityFrame.pack()
+    activityFrame.config(bg="white")
+    HeadLabel = Label(activityFrame, text="Current Activity:", font =('arial', 10, 'normal'), bg="white", fg="black")
+    HeadLabel.grid(row=0, column=0, padx=10, pady=10)
+    
+    buttonMenu = OptionMenu(activityFrame, clicked, *topics)
+    buttonMenu.config( bg="white", fg="black", font = ('arial', 12, 'normal'), activebackground="black")
+    buttonMenu.grid(row=0, column=1, padx=10, pady=10)
+
+    statusLabel = Label(activityFrame, text="Status:", font =('arial', 12, 'normal'), bg="white", fg="black")
+    statusLabel.grid(row=0, column=2, padx=10, pady=10)
+    statusoutLabel = Label(activityFrame, text="########", font =('arial', 12, 'normal'), bg="white", fg="black")
+    statusoutLabel.grid(row=0, column=3, padx=10, pady=10)
+
+    actionButton = Button(activityFrame, text="Start", command=lambda: actionButton_command(clicked, actionButton),
+                            width = 10, height = 1,bg="white", fg="black", 
+                            font = ('arial', 10, 'bold'), activebackground="black", activeforeground="white")
+    actionButton.grid(row=1, column=1, padx=10, pady=10)
+
+    endDayButton = Button(activityFrame, text="End Day", command=lambda: endDay(),
+                            width = 10, height = 1,bg="white", fg="black", 
+                            font = ('arial', 10, 'bold'), activebackground="black", activeforeground="white")
+    endDayButton.grid(row=1, column=2, padx=10, pady=10)
+
+
+    #Frame to display the Activities
+    topicFrame = Frame(root)
+    topicFrame.pack()
+    topicFrame.config(bg="white")
+
+    Label(topicFrame, text="Topic", font =('arial', 14, 'bold'), bg="white", fg="black").grid(row=0, column=0, sticky=W, padx=40)
+    Label(topicFrame, text="Status", font =('arial', 14, 'bold'), bg="white", fg="black").grid(row=0, column=1, sticky=W, padx=20)
+    Label(topicFrame, text="Time", font =('arial', 14, 'bold'), bg="white", fg="black").grid(row=0, column=2, sticky=W, padx=20)
+    
+    status = []
+    time = []
+
+    for i in range(len(topics)):
+        Label(topicFrame, text=topics[i], font =('arial', 12, 'normal'), bg="white", fg="black").grid(row=i+1, column=0, sticky=W, padx=40)
+        status.append(Label(topicFrame, text=topStatus[i], font =('arial', 12, 'normal'), bg="white", fg="black"))
+        status[i].grid(row=i+1, column=1, sticky=W)
+        time.append(Label(topicFrame, text=topTime[i].strftime("%H:%M:%S"), font =('arial', 12, 'normal'), bg="white", fg="black"))
+        time[i].grid(row=i+1, column=2, sticky=W)
+    
+
+    #Add Topic
+    addTopicFrame = Frame(root)
+    addTopicFrame.pack()
+    addTopicFrame.config(bg="white")
+    addTopicLabel = Label(addTopicFrame, text="Add Topic:", font =('arial', 12, 'bold'), bg="white", fg="black")
+    addTopicLabel.grid(row=0, column=0, padx=10, pady=10)
+    addTopicEntry = Entry(addTopicFrame, width=20, bg="white", fg="white")
+    addTopicEntry.grid(row=0, column=1, padx=10, pady=10)
+    addTopicButton = Button(addTopicFrame, text="Add", command=lambda: addTopic(addTopicEntry),
+                            width = 10, height = 1,bg="white", fg="black", 
+                            font = ('arial', 10, 'bold'), activebackground="black", activeforeground="white")
+    addTopicButton.grid(row=0, column=2, padx=10, pady=10)
+
+    update(dateoutLabel, timeoutLabel, status, time, clicked, statusoutLabel)
+
+
+def update(dateoutLabel, timeoutLabel, status, time, clicked, statusoutLabel):
+    global flag_screen2, startTime, activeTopic, topics, topStatus, topTime
+    if flag_screen2 == 0:
+        return
+    else:
+        now = datetime.datetime.now()
+        nowDate = now.strftime("%d/%m/%Y")
+        nowTime = now.strftime("%I:%M:%S %p")
+        dateoutLabel.config(text=nowDate)
+        timeoutLabel.config(text=nowTime)
+        for i in range(len(topics)):
+            if activeTopic == topics[i]:
+                spentTime = datetime.datetime.now() - startTime
+                hour = (spentTime.days*24) +int(spentTime.seconds/3600) + pastTime.hour
+                minute = int(spentTime.seconds/60) + pastTime.minute
+                seconds = spentTime.seconds + pastTime.second
+                topTime[i] = datetime.time(hour, minute, seconds)
+            
+            if topStatus[i] == "In Progress":
+                status[i].config(text = topStatus[i], font=('arial',12,'bold'), fg="red")
+            else:
+                status[i].config(text=topStatus[i], font=('arial',12,'normal'),fg="black")
+            time[i].config(text=topTime[i])          
+
+            if clicked.get() == topics[i]:
+                statusoutLabel.config(text=topStatus[i])
+                
+
+        root.after(100, lambda: update(dateoutLabel, timeoutLabel, status, time, clicked, statusoutLabel))
+
+
+
+
+def startButton_command():
+    global topics, topStatus, topTime
+    topics = []
+    with open("Topics.csv",'r') as file:
+        while(True):
+            row = file.readline()  
+            if(row == ''):
+                break
+            topics.append(row.removesuffix('\n'))
+    # print(topics)   
+
+    topStatus = []
+    topTime = []
+    
+    for i in range(len(topics)):
+        topStatus.append("Not Started")
+        topTime.append(datetime.time())
+
+    screen2()
+
+def actionButton_command(clicked, actionButton):
+    global On, activeTopic, topics, topStatus, topTime, startTime, pastTime, logFlag
+    if On == True:
+        for i in range(len(topics)):
+            if activeTopic == topics[i]:
+                On = False
+                topStatus[i] = "Done"
+                spentTime = datetime.datetime.now() - startTime
+                hour = (spentTime.days*24) +int(spentTime.seconds/3600) + pastTime.hour
+                minute = int(spentTime.seconds/60) + pastTime.minute
+                seconds = spentTime.seconds + pastTime.second
+                topTime[i] = datetime.time(hour, minute, seconds)
+                actionButton.config(text="Start", fg="black")
+                log(activeTopic, startTime)
+                activeTopic = ""
+                
+    else:
+        for i in range(len(topics)):
+            if clicked.get() == topics[i]:
+                topStatus[i] = "In Progress"
+                activeTopic = topics[i]
+                startTime = datetime.datetime.now()
+                pastTime = topTime[i]
+                On = True
+                actionButton.config(text="Stop", fg="red")
+
+def log(activeTopic, startTime):
+    spentTime = datetime.datetime.now() - startTime
+    hour = (spentTime.days*24) +int(spentTime.seconds/3600)
+    minute = int(spentTime.seconds/60) 
+    seconds = spentTime.seconds 
+    spent = datetime.time(hour, minute, seconds)
+    now = datetime.datetime.now()
+    nowDate = now.strftime("%d/%m/%Y")
+    nowTime = now.strftime("%I:%M:%S %p")
+    write = activeTopic + "," + nowDate + ',' + startTime.strftime("%I:%M:%S %p") + ',' + nowTime + ',' + spent.strftime("%X") + '\n'
+    with open('log.csv', 'a') as f:
+        f.write(write) 
+
+def newlog(newactivity):
+    now = datetime.datetime.now()
+    nowDate = now.strftime("%d/%m/%Y")
+    nowTime = now.strftime("%I:%M:%S %p")
+    temp = datetime.time()
+    write = newactivity + ',' + nowDate + ',' + nowTime + ',' + temp.strftime("%X") + ',' + temp.strftime("%X") + ',' + 'New Addition' +'\n'
+    with open('log.csv', 'a') as f:
+        f.write(write) 
+
+def endDay():
+    global topTime, topics, topStatus
+    with open('EndDay_Log.csv','a') as file:
+        file.write('\n' + "Date" + "," + datetime.datetime.now().strftime("%d/%m/%Y") + '\n')
+        for i in range(len(topics)):
+            file.write(topics[i] + "," + topStatus[i] + ',' + topTime[i].strftime("%X") + '\n')        
+    screen1()
+
+def addTopic(addTopicEntry):
+    global topics, topStatus, topTime
+    topics.append(addTopicEntry.get())
+    newtopic = addTopicEntry.get()
+    with open("Topics.csv",'a') as file:
+        file.write(addTopicEntry.get() + '\n')
+    topStatus.append("Not Started")
+    topTime.append(datetime.time())
+    newlog(newtopic)
+    addTopicEntry.delete(0, END)
+    screen2()
+
+if __name__ == "__main__":
+    screen1()
+    root.mainloop()
